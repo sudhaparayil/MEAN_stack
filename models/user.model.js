ff9260cde23ad6c29b522fbb4a3c46670fafdf76
sudhaparayil <parayilsudha@gmail.com>
2018-10-25 15:44:30 +0530

    backend

---
+++ models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+var userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: 'Full name can\'t be empty'
+    },
+    email: {
+        type: String,
+        required: 'Email can\'t be empty',
+        unique: true
+    },
+    password: {
+        type: String,
+        required: 'Password can\'t be empty',
+        minlength : [8,'Password must be Minimum eight characters, at least one letter, one number and one special character']
+    },
+    saltSecret: String
+});
+
+// Custom validation for fullName
+userSchema.path('fullName').validate((val2) => {
+    fullNameRegex = /^[a-zA-Z]+$/;
+    return fullNameRegex.test(val2);
+}, 'Invalid fullName,only alphabetic characters.');
+
+// Custom validation for email
+userSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
+// Custom validation for password
+userSchema.path('password').validate((val1) => {
+    passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    return passwordRegex.test(val1);
+}, 'Invalid password,Minimum eight characters, at least one letter, one number and one special character.');
+// Events
+userSchema.pre('save', function (next) {
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            this.password = hash;
+            this.saltSecret = salt;
+            next();
+        });
+    });
+});
+
+mongoose.model('User', userSchema);
